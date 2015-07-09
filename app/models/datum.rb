b8b0ef3fb89b5c6473bf39914206ebf2ebb29f1a
Colin Walker <cjwalker@sfu.ca>
2015-07-09 08:22:30 -0700

    add data structure getters to model

--- app/models/datum.rb
+++ app/models/datum.rb
@@ -1,16 +1,41 @@
 class Datum < ActiveRecord::Base
   belongs_to :user
 
+  # ==> DATA STRUCTURE
+  # series[0] => time
+  # series[1] => heart rate
+  # series[2] => accel data
+  # series[3] => stages
+  # series[4] => moving average
+  # series[5] => fixed average
+  # series[6] => moving volatility
+
   def data_time
     series.map{|a| a[0] }
   end
 
   def data_heart
-    series.map{|a| a[1].to_i }
+    series.map{|a| a[1] }
   end
 
   def data_accel
-    series.map{|a| a[2].to_i * 50 - 50 }
+    series.map{|a| a[2] * 50 - 50 }
+  end
+
+  def data_stages
+    series.map{|a| a[3] }
+  end
+
+  def data_mov_average
+    series.map{|a| a[4].to_i }
+  end
+
+  def data_fixed_average
+    series.map{|a| a[5].to_i }
+  end
+
+  def data_moving_volatility
+    series.map{|a| a[6].to_i }
   end
 
 end
