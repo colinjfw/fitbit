e4340e523fc548d648dd7dbb66ff9022a877c3dd
Colin Walker <cjwalker@sfu.ca>
2015-07-08 17:55:44 -0700

    proccessable heart data

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,30 @@ class User < ActiveRecord::Base
     Oauth2Rails::Fitbit.new(self, options)
   end
 
+  def process_heart_data(json1, json2)
+    values1 = json1['activities-heart-intraday']['dataset'].map{ |a| a.values[1] }
+    values2 = json2['activities-heart-intraday']['dataset'].map{ |a| a.values[1] }
+    values = values1 + values2
+    data = {
+      info: {
+        interval: json1['activities-heart-intraday']['datasetInterval'],
+        interval_type: json1['activities-heart-intraday']['datasetType'],
+        start_date: json1['activities-heart'][0]['dateTime'],
+        start_time: json1['activities-heart-intraday']['dataset'][0].values[0],
+        end_time:   json2['activities-heart-intraday']['dataset'].last.values[0]
+      },
+      dataset: values
+    }
+    data.to_json
+  end
+
   def get_data(date)
     data = self.data.find_by(day: date)
     if data.nil?
       fitbit_sleep = self.fitbit.sleep(date).json_body
-      fitbit_heart = self.fitbit.minute_heart(1,1,date,'12:00','12:00').json_body # def minute_heart(days, seconds, start_date, start_time, end_time)
+      heart1 = self.fitbit.minute_heart(1,1,date,'22:00','23:59').json_body
+      heart2 = self.fitbit.minute_heart(1,1,date.to_date + 1,'00:00','10:00').json_body
+      fitbit_heart = process_heart_data(heart1,heart2)
       Datum.create!(
         user_id: self.id,
         day: date,
