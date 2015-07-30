9cede0f7428580138b87debb4020e3b6a5bcc853
Colin Walker <cjwalker@sfu.ca>
2015-07-30 16:32:43 -0700

    update ignore

--- config/initializers/oauth2_rails.rb
+++ config/initializers/oauth2_rails.rb
@@ -1,2 +1,3 @@
-OAUTH2_RAILS_ID = '229MB3'
-OAUTH2_RAILS_SECRET = '63b9c42d78061f6eec4a29e206341dc5'
+OAUTH2_RAILS_ID       = '229MB3'
+OAUTH2_RAILS_SECRET   = '63b9c42d78061f6eec4a29e206341dc5'
+OAUTH2_RAILS_CALLBACK = '/http://fitbit-colin.herokuapp.com/oauth2_callbacks/fitbit'
