8437a7d767f621d59deccf3f9d77707ba264e45b
Colin Walker <cjwalker@sfu.ca>
2015-07-30 16:58:04 -0700

    add as env variables

---
+++ config/initializers/oauth2_rails.rb
@@ -0,0 +1,3 @@
+OAUTH2_RAILS_ID       = ENV['OAUTH2_RAILS_ID']
+OAUTH2_RAILS_SECRET   = ENV['OAUTH2_RAILS_SECRET']
+OAUTH2_RAILS_CALLBACK = 'http://fitbit-colin.herokuapp.com/oauth2_callbacks/fitbit'
